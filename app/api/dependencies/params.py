ae886c5d9438df31f964f05d8fdc3317f936ced1
Andrei Lahunovich <lagunmelman@gmail.com>
2025-04-16 01:52:11 +0300

    вынес зависимости в отдельную папку

---
+++ app/api/dependencies/params.py
@@ -0,0 +1,13 @@
+from typing import Annotated
+from fastapi import Depends, Query
+
+from api.dependencies.dependencies import spimex_crud
+from crud.spimexs import SpimexCRUD
+from schemas.spimex import SpimexFiltersDynamics, SpimexFiltersResults
+
+
+SpimexCrud = Annotated[SpimexCRUD, Depends(spimex_crud)]
+SpimexFiltersD = Annotated[SpimexFiltersDynamics, Depends()]
+SpimexFiltersR = Annotated[SpimexFiltersResults, Depends()]
+LimitDependency = Annotated[int, Query(gt=0, lt=1000)]
+OffsetDependency = Annotated[int, Query(ge=0)]
