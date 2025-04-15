5c9ea7bc2fb9c5734e15eb94652343bac41a9c1b
Andrei Lahunovich <lagunmelman@gmail.com>
2025-04-16 01:51:31 +0300

    добавил api get_trading_results

--- app/api/api_v1/spimexs.py
+++ app/api/api_v1/spimexs.py
@@ -4,8 +4,9 @@ from schemas.spimex import (
     SpimexTradingDate,
     Spimex,
 )
-from api.dependencies.parametrs import (
-    SpimexFilters,
+from api.dependencies.params import (
+    SpimexFiltersD,
+    SpimexFiltersR,
     LimitDependency,
     OffsetDependency,
     SpimexCrud,
@@ -16,11 +17,14 @@ router = APIRouter(
 )
 
 
-@router.get("/last_trading_dates/", response_model=list[SpimexTradingDate])
+@router.get(
+    "/last_trading_dates/",
+    response_model=list[SpimexTradingDate],
+)
 async def get_last_trading_dates(
     crud: SpimexCrud,
-    limit: LimitDependency,
-    offset: OffsetDependency = None,
+    limit: LimitDependency = 100,
+    offset: OffsetDependency = 0,
 ):
     return await crud.get_last_trading_dates(
         limit=limit,
@@ -28,14 +32,34 @@ async def get_last_trading_dates(
     )
 
 
-@router.get("/dynamics/", response_model=list[Spimex])
+@router.get(
+    "/dynamics/",
+    response_model=list[Spimex],
+)
 async def get_dynamics(
     crud: SpimexCrud,
-    filters: SpimexFilters,
-    limit: LimitDependency = None,
-    offset: OffsetDependency = None,
+    filters: SpimexFiltersD,
+    limit: LimitDependency = 100,
+    offset: OffsetDependency = 0,
 ):
     return await crud.get_dynamics(
+        filters=filters,
+        limit=limit,
+        offset=offset,
+    )
+
+
+@router.get(
+    "/results/",
+    response_model=list[Spimex],
+)
+async def get_trading_results(
+    crud: SpimexCrud,
+    filters: SpimexFiltersR,
+    limit: LimitDependency = 100,
+    offset: OffsetDependency = 0,
+):
+    return await crud.get_trading_results(
         filters=filters,
         limit=limit,
         offset=offset,
