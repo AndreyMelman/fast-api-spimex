acca7db47d8d1a0419db3cac2f4040392ab0b0cb
Andrei Lahunovich <lagunmelman@gmail.com>
2025-04-15 21:52:21 +0300

    Конфиг для приложения

---
+++ app/core/config.py
@@ -0,0 +1,72 @@
+from datetime import datetime
+
+from pydantic import (
+    BaseModel,
+    PostgresDsn,
+)
+from pydantic_settings import (
+    BaseSettings,
+    SettingsConfigDict,
+)
+
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+class ApiV1Prefix(BaseModel):
+    prefix: str = "/v1"
+    spimex: str = "/spimex"
+
+
+class ApiPrefix(BaseModel):
+    prefix: str = "/api"
+    v1: ApiV1Prefix = ApiV1Prefix()
+
+
+class RunConfig(BaseModel):
+    host: str = "localhost"
+    port: int = 8000
+
+
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    max_overflow: int = 10
+    pool_size: int = 50
+
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
+
+class Config(BaseModel):
+    base_url: str = "https://spimex.com/markets/oil_products/trades/results/"
+    download_dir: str = "downloads/"
+    target_date: datetime = datetime(2023, 1, 1)
+    concurrent_downloads: int = 200
+    max_retries: int = 3
+    concurrent_processing: int = 100
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=BASE_DIR / ".env",
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+        env_file_encoding="utf-8",
+    )
+
+    db: DatabaseConfig
+    cf: Config = Config()
+    run: RunConfig = RunConfig()
+    api: ApiPrefix = ApiPrefix()
+
+
+settings = Settings()
