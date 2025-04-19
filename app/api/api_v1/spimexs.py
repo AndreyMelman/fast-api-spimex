499a8c1791030f147af0ffc28dfc557322abb0a2
Andrei Lahunovich <lagunmelman@gmail.com>
2025-04-19 14:00:15 +0300

    Добавил пагинацию limit и offset в api эндпоинты

--- app/api/api_v1/spimexs.py
+++ app/api/api_v1/spimexs.py
@@ -7,11 +7,8 @@ from schemas.spimex import (
 from api.dependencies.params import (
     SpimexFiltersD,
     SpimexFiltersR,
-    LimitDependency,
-    OffsetDependency,
+    PaginationDep,
     SpimexCrud,
-    CURRENT_LIMIT,
-    CURRENT_OFFSET,
 )
 from core.redis import redis_client
 from services.cache import redis_cache
@@ -29,20 +26,19 @@ router = APIRouter(
 @redis_cache(redis_client, ttl=get_seconds_until_1411())
 async def get_last_trading_dates(
     crud: SpimexCrud,
-    limit: LimitDependency = CURRENT_LIMIT,
-    offset: OffsetDependency = CURRENT_OFFSET,
+    pagination: PaginationDep,
 ):
     """
     Эндпоинт для получения последних дат торгов
 
     :param crud: сессия
-    :param limit: обязательный параметр для получения последних дат
-    :param offset: для пагинации
+    :param pagination: принимает 2 параметра limit и offset,
+    limit обязательный параметр для получения последних дат
     :return: список дат в формате JSON
     """
     return await crud.get_last_trading_dates(
-        limit=limit,
-        offset=offset,
+        limit=pagination.limit,
+        offset=pagination.offset,
     )
 
 
@@ -54,22 +50,21 @@ async def get_last_trading_dates(
 async def get_dynamics(
     crud: SpimexCrud,
     filters: SpimexFiltersD,
-    limit: LimitDependency = CURRENT_LIMIT,
-    offset: OffsetDependency = CURRENT_OFFSET,
+    pagination: PaginationDep,
 ):
     """
     Эндпоинт для получения фильтрованных данных торгов по дате
 
     :param crud: сессия
     :param filters: фильтрация данных, обязательные параметры start_date и end_date
-    :param limit: параметр для получения последних дат
-    :param offset: для пагинации
+    :param pagination: принимает 2 параметра limit и offset,
+    limit обязательный параметр для получения последних дат
     :return: возвращает список отфильтрованных данных в формате JSON
     """
     return await crud.get_dynamics(
         filters=filters,
-        limit=limit,
-        offset=offset,
+        limit=pagination.limit,
+        offset=pagination.offset,
     )
 
 
@@ -81,19 +76,19 @@ async def get_dynamics(
 async def get_trading_results(
     crud: SpimexCrud,
     filters: SpimexFiltersR,
-    limit: LimitDependency = CURRENT_LIMIT,
-    offset: OffsetDependency = CURRENT_OFFSET,
+    pagination: PaginationDep,
 ):
     """
     Эндпоинт для получения фильтрованных данных торгов за последнюю дату
+
     :param crud: сессия
-    :param filters: фильтрация данных, нет обязательных параметров
-    :param limit: параметр для получения последних дат
-    :param offset: для пагинации
+    :param filters: фильтрация данных, обязательные параметры start_date и end_date
+    :param pagination: принимает 2 параметра limit и offset,
+    limit обязательный параметр для получения последних дат
     :return: возвращает список отфильтрованных данных в формате JSON
     """
     return await crud.get_trading_results(
         filters=filters,
-        limit=limit,
-        offset=offset,
+        limit=pagination.limit,
+        offset=pagination.offset,
     )
