f31df6a031b4a0a506f42d7756d29ed5573e4359
Andrei Lahunovich <lagunmelman@gmail.com>
2025-04-19 14:00:37 +0300

    Сделал зависимость для пагинации limit и offset

--- app/api/dependencies/params.py
+++ app/api/dependencies/params.py
@@ -1,18 +1,15 @@
 from typing import Annotated
-from fastapi import Depends, Query
+from fastapi import Depends
 
 from api.dependencies.dependencies import spimex_crud
 from crud.spimexs import SpimexCRUD
 from schemas.spimex import (
     SpimexFiltersDynamics,
     SpimexFiltersResults,
+    Pagination,
 )
-
-CURRENT_LIMIT = 100
-CURRENT_OFFSET = 0
 
 SpimexCrud = Annotated[SpimexCRUD, Depends(spimex_crud)]
 SpimexFiltersD = Annotated[SpimexFiltersDynamics, Depends()]
 SpimexFiltersR = Annotated[SpimexFiltersResults, Depends()]
-LimitDependency = Annotated[int, Query(gt=0, lt=1000)]
-OffsetDependency = Annotated[int, Query(ge=0)]
+PaginationDep = Annotated[Pagination, Depends()]
